1aebfe39f3ad0e7832ad8cc645672f8dc76e2887
Andrés Yáñez <ndrs.yanez@gmail.com>
2020-09-23 12:43:32 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './images/card2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          <img src={image} alt="card"></img>
+          <img src={image} alt="card"></img>
+          <img src={image} alt="card"></img>
+          <img src={image} alt="card"></img>
+        </div>
       </header>
     </div>
   );
